d91c51171c291045275ce44ae3d348e7d03cd589
jennifer <jennifer.malleret@outlook.fr>
2019-07-23 09:17:56 +0200

    Ajout de l'exercice 6 js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function valider(){
+  var saisie = document.getElementById('number').value;
+  window.alert(saisie * 10);
+  }
+
+function emptyField(){
+  document.getElementById('number').value='';
+}
+
+var number = document.querySelector ('input').value;
